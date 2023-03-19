cdd8d9e1af12d0a5354e7f3fbe1307622a12bcd5
Peterson Kabanda <pkabanda82@gmail.com>
2023-03-19 11:41:03 -0700

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,8 +1,8 @@
 #include <stdio.h>
 
 /**
- * main -This is the main function
- *Return: always 0
+ *main - prints "Programming is like building a multilingual puzzle, followed by a new line."
+ *return 0.
  */
 
 int main(void)
